efd8d7ef7ad79fd978475210b949a627b6b5a2ae
stan_wang <a7569209@gmail.com>
2021-02-12 17:15:21 +0800

    Show related comments on restaurant page & using moment

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -56,8 +58,12 @@ const restController = {
 
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
     }).then(restaurant => {
+      console.log(restaurant.Comments[0].dataValues)
       return res.render('restaurant', {
         restaurant: restaurant.toJSON()
       })
